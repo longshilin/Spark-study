e7ed6facb0ecb00c0ad029e11e51b9a42ddd590f
elon <yilong0722@gmail.com>
2018-01-25 02:44:35 +0800

    update

---
+++ src/main/scala/scala/function/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 这个包是用来表示scala中的function函数、方法的实例，是自己学习Scala编程过程中的总结与实践
+ */
+/**
+ * @author elon@elon33.com
+ * {@link blog.csdn.net/Coder__CS}
+ *
+ */
+package scala.function;
--- src/main/scala/scala/wordcount.scala
+++
@@ -1,5 +0,0 @@
-package scala
-
-object wordcount {
-  
-}
